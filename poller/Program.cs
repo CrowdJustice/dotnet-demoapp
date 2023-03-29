b2e4bfd3d071aa579aecbbcf0803296a35942f3d
tjweldon <thomas.weldon@legl.com>
2023-03-29 15:02:40 +0100

    added example poller with docker build

---
+++ poller/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace poller;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+		Program.Poll();
+    }
+    static void Poll() {
+		// In production code, don't destroy the HttpClient through using, but better use 
+		// IHttpClientFactory factory or at least reuse an existing HttpClient instance
+		// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests
+		// https://www.aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/
+		using (var httpClient = new HttpClient())
+		{
+    		using (var request = new HttpRequestMessage(new HttpMethod("GET"), "http://www.example.com/"))
+    		{
+        		var response = httpClient.Send(request);
+        		Console.WriteLine(response.ToString());
+    		}
+		}
+    }
+}
